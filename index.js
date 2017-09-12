f350775bdce6468ddfaef7577283bfa4265b880e
Naveen kumar <naveenkumar.banajar@gmail.com>
2017-09-12 07:27:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
-const two = 2  
+const two = 2
 return n + two
 
   // Feel free to move things around!
-  
+
 }
 
 var funkyFunction = function() {
